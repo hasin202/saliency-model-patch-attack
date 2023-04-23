743627d7ef2a0295592bf760dcf8fbfdede867a9
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-04-23 14:30:49 +0100

    removed destroy all call

--- utils/display.py
+++ utils/display.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 def display(window_name, image):
@@ -7,6 +8,3 @@ def display(window_name, image):
         k = cv2.waitKey(1) & 0xFF
         if k == 27:
             break
-
-
-cv2.destroyAllWindows()
